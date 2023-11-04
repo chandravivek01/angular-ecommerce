25f203fc4b790d936d3a8af1a2c8c1fd22749a78
Vivek Chandra Sardar <chandravivek52@gmail.com>
2023-11-04 14:47:28 +0530

    create checkout-component, stage-1: checkout-form-layout ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,11 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 const routes : Routes = [
+  {path: 'checkout', component: CheckoutComponent },
   {path: 'cart-details', component: CartDetailsComponent },
   {path: 'search/:keyword', component: ProductListComponent },
   {path: 'category/:id/:name', component: ProductListComponent },
@@ -31,13 +34,15 @@ const routes : Routes = [
     SearchComponent,
     ProductDetailsComponent,
     CartStatusComponent,
-    CartDetailsComponent
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
