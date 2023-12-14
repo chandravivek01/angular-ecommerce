b3ed99e31632576361f8376ec9914ad077a6444c
Vivek Chandra Sardar <chandravivek52@gmail.com>
2023-12-14 16:19:26 +0530

    set up https(TSL) ...

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oad8grfe4y0RaMRF5d7',
         issuer: 'https://dev-88711623.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     } 
 }
